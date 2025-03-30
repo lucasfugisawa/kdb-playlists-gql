d20f473d08a2cdc841ad4ff446419888acd2f4c2
Lucas Fugisawa <lucasfugisawa@gmail.com>
2025-03-30 09:51:01 -0300

    Upgrade dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ val graphql_kotlin_version: String = "8.4.0"
 
 plugins {
     kotlin("jvm") version "2.1.20"
-    id("io.ktor.plugin") version "3.1.1"
+    id("io.ktor.plugin") version "3.1.2"
     id("org.jmailen.kotlinter") version "5.0.1"
 }
 
@@ -34,32 +34,28 @@ repositories {
 }
 
 dependencies {
-    implementation("io.ktor:ktor-server-core-jvm")
-    implementation("io.ktor:ktor-server-netty")
-    implementation("ch.qos.logback:logback-classic:$logback_version")
-    implementation("io.ktor:ktor-server-core")
     implementation("io.ktor:ktor-server-config-yaml")
+    implementation("io.ktor:ktor-server-core")
+    implementation("io.ktor:ktor-server-core-jvm")
+    implementation("io.ktor:ktor-server-cors")
+    implementation("io.ktor:ktor-server-host-common")
+    implementation("io.ktor:ktor-server-netty")
+    implementation("io.ktor:ktor-server-status-pages")
 
-    // Database
-    implementation("org.postgresql:postgresql:$postgres_version")
+    implementation("ch.qos.logback:logback-classic:$logback_version")
     implementation("com.zaxxer:HikariCP:$hikaricp_version")
 
-    // Exposed
+    implementation("org.postgresql:postgresql:$postgres_version")
+
     implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
     implementation("org.jetbrains.exposed:exposed-dao:$exposed_version")
     implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
     implementation("org.jetbrains.exposed:exposed-java-time:$exposed_version")
 
-    // Flyway
     implementation("org.flywaydb:flyway-core:$flyway_version")
     implementation("org.flywaydb:flyway-database-postgresql:$flyway_version")
 
-    // GraphQL-Kotlin
     implementation("com.expediagroup:graphql-kotlin-ktor-server:$graphql_kotlin_version")
-
-    implementation("io.ktor:ktor-server-cors:3.1.1")
-    implementation("io.ktor:ktor-server-host-common:3.1.1")
-    implementation("io.ktor:ktor-server-status-pages:3.1.1")
 
     testImplementation("io.ktor:ktor-server-test-host")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
